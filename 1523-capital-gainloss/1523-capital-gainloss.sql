8a7078cc21e31d5b5c77317f41c2fd8d3ae32295
Henos78 <90530676+Henos78@users.noreply.github.com>
2025-06-23 15:35:39 +0300

    Time: 449 ms (97.33%), Space: 0B (100%) - LeetHub

--- 1523-capital-gainloss/1523-capital-gainloss.sql
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -1,11 +1,9 @@
 # Write your MySQL query statement below
-with gain_loss  as (
-    select stock_name, SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END) - 
-        SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) as net 
-    from stocks group by stock_name
-)
-select s.stock_name, g.net as capital_gain_loss from stocks s
-join gain_loss g on g.stock_name=s.stock_name
-group by s.stock_name
+
+select stock_name, SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END) - 
+SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) as capital_gain_loss 
+from stocks group by stock_name
+
+
 
 
