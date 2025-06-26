01ca8a7dd0d3871280e6e790d7ee2f1fbd7e4368
Henos78 <90530676+Henos78@users.noreply.github.com>
2025-06-27 01:11:02 +0300

    Time: 457 ms (72.15%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,13 +1,5 @@
 # Write your MySQL query statement below
-#First trail but didnt pass all the test case
-/**with temp as (
-    select id, temperature,
-    lag(temperature) over ( order by recordDate) as prev_temp from weather
-)
 
-select id from temp
-where temperature > prev_temp
-**/
 
  select today.id  from weather today
  cross join weather yesterday 
